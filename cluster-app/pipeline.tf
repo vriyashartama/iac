514b41057645ff67d8a6058357f9d7a61a1986aa
Vriyas Hartama <hi@vriyas.com>
2022-06-24 23:57:02 +0700

    update

--- cluster-app/pipeline.tf
+++ cluster-app/pipeline.tf
@@ -72,161 +72,161 @@ resource "kubectl_manifest" "whoami" {
 }
 
 
-# provider "helm" {
-#   kubernetes {
-#     config_path = "../kubeconfig"
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    config_path = "../kubeconfig"
+  }
+}
 
-# resource "random_string" "drone_rpc_secret" {
-#   length = 32
-#   special = false
-# }
+resource "random_string" "drone_rpc_secret" {
+  length = 32
+  special = false
+}
 
-# resource "random_string" "minio_access_key" {
-#   length = 32
-#   special = false
-# }
+resource "random_string" "minio_access_key" {
+  length = 32
+  special = false
+}
 
-# resource "random_string" "harbor_admin_password" {
-#   length = 16
-#   special = false
-# }
+resource "random_string" "harbor_admin_password" {
+  length = 16
+  special = false
+}
 
 
-# resource "random_string" "harbor_secret_key" {
-#   length = 16
-#   special = false
-# }
+resource "random_string" "harbor_secret_key" {
+  length = 16
+  special = false
+}
 
-# resource "random_uuid" "minio_secret_key" {
-# }
+resource "random_uuid" "minio_secret_key" {
+}
 
-# data "kubectl_path_documents" "default" {
-#   pattern = "${path.module}/manifest/*.yaml"
-#   vars = {
-#       azurestorageaccountname = base64encode(data.terraform_remote_state.k3s.outputs.azure_storage_name)
-#       azurestorageaccountkey = base64encode(data.terraform_remote_state.k3s.outputs.azure_file_share_key)
-#       azure_storage_name = data.terraform_remote_state.k3s.outputs.azure_storage_name
-#     }
-# }
+data "kubectl_path_documents" "default" {
+  pattern = "${path.module}/manifest/*.yaml"
+  vars = {
+      azurestorageaccountname = base64encode(data.terraform_remote_state.k3s.outputs.azure_storage_name)
+      azurestorageaccountkey = base64encode(data.terraform_remote_state.k3s.outputs.azure_file_share_key)
+      azure_storage_name = data.terraform_remote_state.k3s.outputs.azure_storage_name
+    }
+}
 
-# data "kubectl_path_documents" "default-count-hack" {
-#   pattern = "${path.module}/manifest/*.yaml"
-#   vars = {
-#       azurestorageaccountname = ""
-#       azurestorageaccountkey = ""
-#       azure_storage_name = ""
-#     }
-# }
+data "kubectl_path_documents" "default-count-hack" {
+  pattern = "${path.module}/manifest/*.yaml"
+  vars = {
+      azurestorageaccountname = ""
+      azurestorageaccountkey = ""
+      azure_storage_name = ""
+    }
+}
 
-# output "harbor_admin_password" {
-#   value = random_string.harbor_admin_password.id
-# }
+output "harbor_admin_password" {
+  value = random_string.harbor_admin_password.id
+}
 
-# output "harbor_secret_key" {
-#   value = random_string.harbor_secret_key.id
-# }
+output "harbor_secret_key" {
+  value = random_string.harbor_secret_key.id
+}
 
-# output "minio_access_key" {
-#   value = random_string.minio_access_key.id
-# }
+output "minio_access_key" {
+  value = random_string.minio_access_key.id
+}
 
-# output "minio_secret_key" {
-#   value = random_uuid.minio_secret_key.id
-# }
+output "minio_secret_key" {
+  value = random_uuid.minio_secret_key.id
+}
 
-# resource "kubectl_manifest" "default" {
-#   count     = length(data.kubectl_path_documents.default-count-hack.documents)
-#   yaml_body = element(data.kubectl_path_documents.default.documents, count.index)
-# }
+resource "kubectl_manifest" "default" {
+  count     = length(data.kubectl_path_documents.default-count-hack.documents)
+  yaml_body = element(data.kubectl_path_documents.default.documents, count.index)
+}
 
-# data "kubectl_path_documents" "drone" {
-#     pattern = "${path.module}/manifest/drone/*.yaml"
-#     vars = {
-#         host = "ci.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#         github_client_id = var.ghClientId
-#         github_client_secret = var.ghClientSecret
-#         drone_rpc_secret = random_string.drone_rpc_secret.id
-#         drone_server_proto = "http"
-#         drone_user_create = "username:${var.droneGhAdmin},admin:true"
-#         drone_user_filter = var.droneGhOrg
-#     }
-# }
+data "kubectl_path_documents" "drone" {
+    pattern = "${path.module}/manifest/drone/*.yaml"
+    vars = {
+        host = "ci.${data.terraform_remote_state.k3s.outputs.public_domain}"
+        github_client_id = var.ghClientId
+        github_client_secret = var.ghClientSecret
+        drone_rpc_secret = random_string.drone_rpc_secret.id
+        drone_server_proto = "http"
+        drone_user_create = "username:${var.droneGhAdmin},admin:true"
+        drone_user_filter = var.droneGhOrg
+    }
+}
 
-# data "kubectl_path_documents" "drone-count-hack" {
-#     pattern = "${path.module}/manifest/drone/*.yaml"
-#     vars = {
-#         host = ""
-#         github_client_id = ""
-#         github_client_secret = ""
-#         drone_rpc_secret = ""
-#         drone_server_proto = ""
-#         drone_user_create = ""
-#         drone_user_filter = ""
-#     }
-# }
+data "kubectl_path_documents" "drone-count-hack" {
+    pattern = "${path.module}/manifest/drone/*.yaml"
+    vars = {
+        host = ""
+        github_client_id = ""
+        github_client_secret = ""
+        drone_rpc_secret = ""
+        drone_server_proto = ""
+        drone_user_create = ""
+        drone_user_filter = ""
+    }
+}
 
-# resource "kubectl_manifest" "drone" {
-#   count     = length(data.kubectl_path_documents.drone-count-hack.documents)
-#   yaml_body = element(data.kubectl_path_documents.drone.documents, count.index)
-# }
+resource "kubectl_manifest" "drone" {
+  count     = length(data.kubectl_path_documents.drone-count-hack.documents)
+  yaml_body = element(data.kubectl_path_documents.drone.documents, count.index)
+}
 
-# data "kubectl_path_documents" "drone_runner" {
-#     pattern = "${path.module}/manifest/drone-runner/*.yaml"
-#     vars = {
-#         host = "ci.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#         drone_rpc_secret = random_string.drone_rpc_secret.id
-#         drone_rpc_proto = "http"
-#     }
-# }
+data "kubectl_path_documents" "drone_runner" {
+    pattern = "${path.module}/manifest/drone-runner/*.yaml"
+    vars = {
+        host = "ci.${data.terraform_remote_state.k3s.outputs.public_domain}"
+        drone_rpc_secret = random_string.drone_rpc_secret.id
+        drone_rpc_proto = "http"
+    }
+}
 
-# data "kubectl_path_documents" "drone_runner-count-hack" {
-#     pattern = "${path.module}/manifest/drone-runner/*.yaml"
-#     vars = {
-#         host = ""
-#         drone_rpc_secret = ""
-#         drone_rpc_proto = ""
-#     }
-# }
+data "kubectl_path_documents" "drone_runner-count-hack" {
+    pattern = "${path.module}/manifest/drone-runner/*.yaml"
+    vars = {
+        host = ""
+        drone_rpc_secret = ""
+        drone_rpc_proto = ""
+    }
+}
 
-# resource "kubectl_manifest" "drone_runner" {
-#   count     = length(data.kubectl_path_documents.drone_runner-count-hack.documents)
-#   yaml_body = element(data.kubectl_path_documents.drone_runner.documents, count.index)
-# }
+resource "kubectl_manifest" "drone_runner" {
+  count     = length(data.kubectl_path_documents.drone_runner-count-hack.documents)
+  yaml_body = element(data.kubectl_path_documents.drone_runner.documents, count.index)
+}
 
-# resource "helm_release" "minio" {
-#   name = "minio"
-#   repository = "https://charts.min.io"
-#   namespace = "pipeline"
-#   chart = "minio"
+resource "helm_release" "minio" {
+  name = "minio"
+  repository = "https://charts.min.io"
+  namespace = "pipeline"
+  chart = "minio"
 
-#   values      = [
-#     templatefile("${path.module}/manifest/minio/01-values.yaml", {
-#       minio_access_key = random_string.minio_access_key.id
-#       minio_secret_key = random_uuid.minio_secret_key.id
-#       minio_host = "storage.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#       minio_console_host = "storage-console.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#     })
-#   ]
-# }
+  values      = [
+    templatefile("${path.module}/manifest/minio/01-values.yaml", {
+      minio_access_key = random_string.minio_access_key.id
+      minio_secret_key = random_uuid.minio_secret_key.id
+      minio_host = "storage.${data.terraform_remote_state.k3s.outputs.public_domain}"
+      minio_console_host = "storage-console.${data.terraform_remote_state.k3s.outputs.public_domain}"
+    })
+  ]
+}
 
-# resource "helm_release" "harbor" {
-#   name = "harbor"
-#   repository = "https://helm.goharbor.io"
-#   namespace = "pipeline"
-#   chart = "harbor"
+resource "helm_release" "harbor" {
+  name = "harbor"
+  repository = "https://helm.goharbor.io"
+  namespace = "pipeline"
+  chart = "harbor"
 
-#   values      = [
-#     templatefile("${path.module}/manifest/harbor/01-values.yaml", {
-#       core_host = "registry.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#       notary_host = "notary.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#       minio_host = "storage.${data.terraform_remote_state.k3s.outputs.public_domain}"
-#       minio_access_key = random_string.minio_access_key.id
-#       minio_secret_key = random_uuid.minio_secret_key.id
-#       minio_bucket = "registry"
-#       harbor_admin_password = random_string.harbor_admin_password.id
-#       harobo_secret_key = random_string.harbor_secret_key.id
-#     })
-#   ]
-# }
+  values      = [
+    templatefile("${path.module}/manifest/harbor/01-values.yaml", {
+      core_host = "registry.${data.terraform_remote_state.k3s.outputs.public_domain}"
+      notary_host = "notary.${data.terraform_remote_state.k3s.outputs.public_domain}"
+      minio_host = "storage.${data.terraform_remote_state.k3s.outputs.public_domain}"
+      minio_access_key = random_string.minio_access_key.id
+      minio_secret_key = random_uuid.minio_secret_key.id
+      minio_bucket = "registry"
+      harbor_admin_password = random_string.harbor_admin_password.id
+      harobo_secret_key = random_string.harbor_secret_key.id
+    })
+  ]
+}
